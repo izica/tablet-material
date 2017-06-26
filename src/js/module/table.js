edfe71da6bca21159f5d4ceb7ddc979a7dc72b4c
izica <artemiztomska@gmail.com>
2017-06-26 17:14:23 +0700

    slider done

--- src/js/module/table.js
+++ src/js/module/table.js
@@ -1,4 +1,19 @@
 jQuery(document).ready(function($) {
+
+    $(".table-body__cell--slider").each(function(index, el) {
+        var slider = $(this).append('<div><div>').find('div')[0];
+        noUiSlider.create(slider, {
+            start: 30,
+            connect: [true, false],
+            range: {
+            	'min': 0,
+            	'max': 100
+            }
+        });
+        
+    });
+
+
     $(".table-head__cell").each(function(index, el) {
         $(this).width($(this).attr('width'));
     });
