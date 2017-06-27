b5570dc1e43383c70fb791cabfe05c5fc258df43
izica <artemiztomska@gmail.com>
2017-06-27 15:06:17 +0700

    table scroll added

--- src/js/module/table.js
+++ src/js/module/table.js
@@ -1,4 +1,13 @@
 jQuery(document).ready(function($) {
+    //table init
+    var row = $(".table-body").html();
+    for (var i = 0; i < 25; i++) {
+        console.log(22);
+        $(".table-body").append(row);
+    }
+
+    var table_head_absolute = $("<div></div>").addClass('table-head').addClass('table-head--absolute').append($('.table-head').html());
+    $(".table-wrap").prepend(table_head_absolute);
 
     $(".table-body__cell--slider").each(function(index, el) {
         var slider = $(this).append('<div><div>').find('div')[0];
@@ -10,11 +19,13 @@ jQuery(document).ready(function($) {
             	'max': 100
             }
         });
-        
-    });
 
+    });
 
     $(".table-head__cell").each(function(index, el) {
         $(this).width($(this).attr('width'));
+        $(this).width($(this).attr('width'));
     });
+
+
 });
